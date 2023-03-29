7067b02983a38865f887065583d61aca2e2e7a02
haimnam <haimnam10@gmail.com>
2023-03-29 23:16:02 +0900

    Create / CRA

---
+++ fe/unicoop/src/Pages/Home/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.body}>
+        <div className={styles.title}>
+          <div className={styles.logo}>UNICOOP</div>
+          <div className={styles.text}>
+            대학교 팀 프로젝트 빌딩 및 협업 플랫폼
+          </div>
+        </div>
+        <div className={styles.join}>
+          <button className={styles.joinButton}>지금 등록하러 가기</button>
+        </div>
+      </div>
+      <div className={styles.footer}></div>
+    </div>
+  );
+};
+
+export default Home;
