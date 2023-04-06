dbe4fc9f2168db84623935d7eac589cf105625a2
chojbsoft <chojbsoft@gmail.com>
2023-04-06 14:08:02 +0900

    feat: 회원탈퇴 api 추가

--- be/index.js
+++ be/index.js
@@ -350,6 +350,61 @@ router.post('/login', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /users/{email}:
+ *   delete:
+ *     tags:
+ *       - auth
+ *     summary: 회원 탈퇴
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         in: path
+ *         description: 삭제할 유저의 이메일
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 유저 삭제 성공
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: User deleted successfully
+ *       404:
+ *         description: 유저가 존재하지 않음
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: User not found
+ *       500:
+ *         description: 서버 내부 오류
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Internal Server Error
+ */
+router.delete('/users/:email', async (req, res) => {
+    try {
+        const { email } = req.params;
+        const deletedUser = await User.findOneAndDelete({ email });
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
 
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////// HELPER ///////////////////////////////////////
