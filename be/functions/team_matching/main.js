c6e4d2185d5355ea2bfd8875a1dd39c0b36b3277
juns98 <64398993+juns98@users.noreply.github.com>
2023-04-14 00:35:50 +0900

    feat: updating algorithm (in progress)

--- be/functions/team_matching/main.js
+++ be/functions/team_matching/main.js
@@ -1,2 +1,13 @@
 const createGroup = require("./createGroupUsingGraph/createGroupTest");
+const createGraph = require("./createGraph/createGraph");
+const getRandomUsers = require("./createGraph/createUsers");
+const questions = require("../../data/DefaultQuestionLists.json").questions;
+
+const users = getRandomUsers(10, questions);
+for (let i = 0; i < users.length; i++) {
+  console.log("user ", i + 1, users[i].answers);
+}
+
+createGraph(users);
+
 createGroup();
