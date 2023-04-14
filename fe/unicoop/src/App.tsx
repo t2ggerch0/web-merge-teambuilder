fe680a071dcd7edcbb1ffcaa781ed77525d6405b
haimnam <haimnam10@gmail.com>
2023-04-14 14:18:34 +0900

    Add/react-router-dom

--- fe/unicoop/src/App.tsx
+++ fe/unicoop/src/App.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./App.module.scss";
 import Home from "./Pages/Home/Home";
+import MyPage from "./Pages/MyPage/MyPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
+  const appRef = useRef<HTMLDivElement | null>(null);
   return (
-    <div className={styles.app}>
-      <Home />
+    <div className={styles.app} ref={appRef}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
