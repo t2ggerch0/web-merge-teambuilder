68bbb5040c1fa00ebc343e17731c898d244b74de
chojbsoft <chojbsoft@gmail.com>
2023-04-20 17:28:44 +0900

    fix: mongoose 임포트

--- be/routes/class/getClass.js
+++ be/routes/class/getClass.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
 const dotenv = require("dotenv");
 dotenv.config();
-const User = require("../../models/User");
+const mongoose = require('mongoose');
+
 const Class = require("../../models/Class");
-const verifyJwt = require("../../utils/verifyJwt");
 
 router.get("/", async (req, res) => {
   try {
     const { classId } = req.query;
 
     // find class with classID
-    const targetClass = await Class.findById(mongoose.Types.ObjectId(classId));
+    const targetClass = await Class.findOne({ _id: mongoose.Types.ObjectId(classId) });
     res.status(201).json({ class: targetClass, code: 1 });
   } catch (error) {
     console.error(error);
