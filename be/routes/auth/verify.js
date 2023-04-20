6677f757bba783769e4eff9b833e5050c0498e49
chojbsoft <chojbsoft@gmail.com>
2023-04-20 14:07:47 +0900

    feat: bcrypt 모듈 추가

--- be/routes/auth/verify.js
+++ be/routes/auth/verify.js
@@ -1,71 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const dotenv = require("dotenv");
-dotenv.config();
+const bcrypt = require("bcryptjs");
 
 const User = require("../../models/User");
 
-/**
- * @swagger
- * /auth/verify:
- *   post:
- *     tags:
- *       - auth
- *     summary: 회원가입
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: body
- *         in: body
- *         description: default유저를 실제 유저로 덮어씁니다.
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             email:
- *               type: string
- *             password:
- *               type: string
- *             userType:
- *               type: string
- *             verifyCode:
- *               type: number
- *             name:
- *               type: string
- *             studentId:
- *               type: number
- *             major:
- *               type: string
- *     responses:
- *       200:
- *         description: 회원가입 성공
- *         schema:
- *           type: object
- *           properties:
- *             code:
- *               type: integer
- *               example: 1
- *       400:
- *         description: 인증코드 불일치
- *         schema:
- *           type: object
- *           properties:
- *             code:
- *               type: integer
- *               example: 0
- *             message:
- *               type: string
- *               example: invalid verify code
- *       500:
- *         description: 서버 내부 오류
- *         schema:
- *           type: object
- *           properties:
- *             message:
- *               type: string
- *               example: Internal Server Error
- */
 router.post("/verify", async (req, res) => {
   try {
     const { email, password, userType, verifyCode, name, studentId, major } = req.body;
