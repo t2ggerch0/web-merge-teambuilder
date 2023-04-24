39d18fbfc5a33fba1cf5b90079e6874f6b71350d
juns98 <64398993+juns98@users.noreply.github.com>
2023-04-25 02:41:48 +0900

    feat: add login test code

--- be/test/login.test.js
+++ be/test/login.test.js
@@ -1,15 +1,46 @@
-// index.js
-require("dotenv").config();
-const mongoose = require("mongoose");
+const request = require("supertest");
+const assert = require("assert");
+const { app, server } = require("../index");
+const { exit } = require("process");
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log("MongoDB connection error:", error));
+describe("API Tests", () => {
+  // Test login endpoint
+  describe("POST /auth/login", () => {
+    it("should return status 200 if email and password are valid", (done) => {
+      const loginData = {
+        email: "juns98@g.skku.edu",
+        password: "1234",
+      };
 
-// create a new user
-const newRequest = new Object({
-  email: "juns98@g.skku.edu",
-  password: "1234",
+      request(app)
+        .post("/auth/login")
+        .send(loginData)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          // assert.strictEqual(res.body.success, true);
+          // assert.strictEqual(typeof res.body.token, "string");
+
+          done();
+        });
+    });
+
+    it("should return status 401 if email or password is invalid", (done) => {
+      const loginData = {
+        email: "test@example.com",
+        password: "invalidpassword",
+      };
+
+      request(app).post("/auth/login").send(loginData).expect(401, done);
+    });
+  });
+});
+
+after(() => {
+  // close connection to app
+  server.close(() => {
+    console.log("Closed Server");
+    exit();
+  });
 });
