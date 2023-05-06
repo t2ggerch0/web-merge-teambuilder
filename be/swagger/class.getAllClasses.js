e55aa3bfb949a848369a8a9b03d107ecb58b7d3c
juns98 <64398993+juns98@users.noreply.github.com>
2023-05-06 19:25:54 +0900

    refactor: update apis

---
+++ be/swagger/class.getAllClasses.js
@@ -0,0 +1,28 @@
+// /**
+//  * @swagger
+//  * /class:
+//  *   get:
+//  *     tags:
+//  *       - class
+//  *     summary: 모든 class 정보를 반환합니다.
+//  *     description: 모든 class 정보를 반환합니다.
+//  *     produces:
+//  *       - application/json
+//  *     parameters:
+//  *       - name: Authorization
+//  *         description: JWT token
+//  *         in: header
+//  *         required: false
+//  *         type: string
+//  *    responses:
+//  *      200:
+//  *         description: Class information retrieved successfully
+//  *      500:
+//  *         description: Internal server error
+//  *         schema:
+//  *            type: object
+//  *            properties:
+//  *               message:
+//  *                  type: string
+//  *                  example: Internal server error
+//  */
