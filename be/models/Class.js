051cf0b8f2e9e034c16d07b3c9b433364c934b5c
juns98 <64398993+juns98@users.noreply.github.com>
2023-05-18 01:52:52 +0900

    feat: update class model

--- be/models/Class.js
+++ be/models/Class.js
@@ -18,10 +18,9 @@ const classSchema = new Schema({
       ref: "Team",
     },
   ],
-  questions: [
+  questionIds: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Question",
+      type: Number,
     },
   ],
   answers: [
