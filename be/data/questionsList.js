a965a3053d6dc6f5f2477cc47dd537955bb3c0be
juns98 <64398993+juns98@users.noreply.github.com>
2023-05-22 00:53:02 +0900

    feat: update questions list

--- be/data/questionsList.js
+++ be/data/questionsList.js
@@ -32,7 +32,7 @@ const questionLists = [
     title: "Preferred Topics",
     options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
     weight: 5,
-    countScore: "different",
+    countScore: "same",
   },
 ];
 
