7985a1abac41ff891e8b350270a0264d6428d542
chojbsoft <chojbsoft@gmail.com>
2023-06-02 12:49:10 +0900

    getTeam 수정

--- be/routes/team/getTeam.js
+++ be/routes/team/getTeam.js
@@ -12,65 +12,53 @@ const Team = require("../../models/Team");
 
 router.get("/", verifyJwt, async (req, res) => {
   try {
-    // // get User
-    // const userId = req.userId;
-    // let targetUser = await User.findById(userId);
+    // get User
+    const userId = req.userId;
+    let targetUser = await User.findById(userId);
 
-    // // get class IDs
-    // let { classId } = req.query;
-    // console.log(classId);
-    // let targetClass = await Class.findById(classId).populate("teams");
-    // let teams = targetClass.teams;
-    // console.log(teams);
+    // get class IDs
+    let { classId } = req.query;
+    console.log(classId);
+    let targetClass = await Class.findById(classId).populate("teams");
+    let teams = targetClass.teams;
+    console.log(teams);
 
-    // // 팀 탐색
-    // let targetTeam = null;
-    // let isFound = false;
-    // for (let i = 0; i < teams.length; i++) {
-    //   if (isFound) {
-    //     break;
-    //   }
+    // 팀 탐색
+    let targetTeam = null;
+    let isFound = false;
+    for (let i = 0; i < teams.length; i++) {
+      if (isFound) {
+        break;
+      }
 
-    //   console.log(teams[i].leader)
-    //   console.log(targetUser._id)
-    //   // // 리더 탐색
-    //   // if (targetUser._id == teams[i].leader) {
-    //   //   targetTeam = teams[i];
-    //   //   break;
-    //   // }
-
-    //   // 멤버 탐색
-    //   let members = teams[i].members;
-    //   // console.log("members: ", members);
-    //   // console.log("userId: ", userId);
-    //   for (let j = 0; j < members.length; j++) {
-    //     if (targetUser._id == members[j]) {
-    //       targetTeam = teams[i];
-    //       isFound = true;
-    //       break;
-    //     }
-    //   }
-    // }
-
-    // if (!targetTeam) {
-    //   return res.status(403).json({ message: "User has no team" });
-    // }
-
-    res.status(201).json({ "targetTeam": {
-      "_id": "60c6f02b33e2cb92c0d0ab3f",
-      "name": "Team A",
-      "leader": "60c6e7c9c9e8435138136d20",
-      "members": [
-        "60c6e7c9c9e8435138136d20"
-      ],
-      "chat": [
-        {
-          "sender": "60c6e7c9c9e8435138136d20",
-          "message": "Hello, team members!",
-          "createdAt": "2023-05-08T12:00:00.000Z"
+      // // 리더 탐색
+      // if (targetUser._id == teams[i].leader) {
+      //   targetTeam = teams[i];
+      //   break;
+      // }
+      
+      // 멤버 탐색
+      let members = teams[i].members;
+      // console.log("members: ", members);
+      // console.log("userId: ", userId);
+      console.log("userID:" + targetUser._id)
+      for (let j = 0; j < members.length; j++) {
+        console.log(targetUser._id);
+        console.log(members[j]._id);
+        if (targetUser._id.toString() == members[j]._id.toString()) {
+          console.log("succ");
+          targetTeam = teams[i];
+          isFound = true;
+          break;
         }
-      ]
-    }});
+      }
+    }
+
+    if (!targetTeam) {
+      return res.status(403).json({ message: "User has no team" });
+    }
+
+    res.status(201).json({ targetTeam});
 
     // let users = [];
     // users.push(targetTeam.members);
