004f52e55b368ab42bdf4e6685fe12f79f67ea93
MyunghaCho <chomyungha@gmail.com>
2023-06-09 04:24:09 +0900

    fincl

--- fe/unicoop/src/Pages/Activity/ActivityChat/ActivityChat.tsx
+++ fe/unicoop/src/Pages/Activity/ActivityChat/ActivityChat.tsx
@@ -3,20 +3,31 @@ import styles from "./ActivityChat.module.scss";
 import ActivityChatInput from "./ActivityChatInput/ActivityChatInput";
 import { useAuthContext } from "../../../Context/UnicoopContext";
 import { useParams } from "react-router-dom";
-import { TeamInfo } from "../../../interface";
+import {
+  ChatInfo,
+  TeamInfo,
+  TeamInfoType,
+  UserContextType,
+} from "../../../interface";
 import { guestApi } from "../../../API/guestApi";
+import dayjs from "dayjs";
+import { teamApi } from "../../../API/teamApi";
 
 type ActivityChatProps = {};
 
 const ActivityChat: FC<ActivityChatProps> = () => {
   const { projectId } = useParams();
   const { myInfo } = useAuthContext();
-  const [teamInfo, setTeamInfo] = useState<{ targetTeam: TeamInfo }>();
+  const [teamInfo, setTeamInfo] = useState<TeamInfoType>();
+  const [chats, setChats] = useState<Array<ChatInfo>>([]);
 
   useEffect(() => {
-    guestApi.getTeamInfo(projectId ?? "", myInfo?.token ?? "").then((res) => {
-      console.log("team info", res);
-      //setTeamInfo(res);
+    teamApi.getClassTeams(projectId ?? "").then((res) => {
+      let data = res.teams as TeamInfoType[];
+      let temp = data.filter((t) =>
+        t.contextByUser.map((u) => u.user).includes(myInfo?.id ?? "")
+      )[0];
+      setTeamInfo(temp);
     });
   }, []);
 
@@ -25,27 +36,25 @@ const ActivityChat: FC<ActivityChatProps> = () => {
       <div className={styles.title}>팀 채팅</div>
 
       <div className={styles.body}>
-        <div className={styles.post}>
-          <div className={styles.postInfo}>
-            <div className={styles.userIcon}>Haim</div>
-            <div className={styles.time}>04/18 11:13 am</div>
+        {chats.map((chat) => (
+          <div className={styles.post}>
+            <div className={styles.postInfo}>
+              <div className={styles.userIcon}>{myInfo?.name}</div>
+              <div className={styles.time}>
+                {dayjs(chat.createdAt).format("MM/DD hh:mm a")}
+              </div>
+            </div>
+            <div className={styles.content}>{chat.message}</div>
+            <div className={styles.comments}>댓글 1개</div>
           </div>
-          <div className={styles.content}>
-            회의한 내용 토대로 추가했습니다. 확인 부탁드려요!
-          </div>
-          <div className={styles.comments}>댓글 1개</div>
-        </div>
-        <div className={styles.post}>
-          <div className={styles.postInfo}>
-            <div className={styles.userIcon}>Joonsun</div>
-            <div className={styles.time}>Yesterday</div>
-          </div>
-          <div className={styles.content}>3슬라이드 수정해서 올려드립니다.</div>
-          <div className={styles.comments}>댓글 2개</div>
-        </div>
-      </div>
+        ))}
 
-      <ActivityChatInput />
+        <ActivityChatInput
+          myTeamId={teamInfo?._id ?? ""}
+          chats={chats}
+          setChats={setChats}
+        />
+      </div>
     </div>
   );
 };
